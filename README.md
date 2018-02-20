222226eebc464b7f774cb948da74349f5d547e1c
philippmuench <philipp.muench@helmholtz-hzi.de>
2018-02-20 17:08:44 +0100

    add command line parameters

--- README.md
+++ README.md
@@ -9,10 +9,28 @@ Biopython
 
 ## prepare reference database
 
-```
+```bash
 cd db
 makeblastdb -in proteobacteria_rep.fasta -dbtype nucl 
 ```
 
 ## usuage
+
+```bash
+>python impute.py --input test.fasta --window 300
+pos: 619 N > A (alignment evalue: 3.73174e-78)
+pos: 677 N > G (alignment evalue: 2.86447e-84)
+
+```
+
+## method
+
+- for every N it will create an alignment to reference database with the windowsize/2 on each direction
+- alignments will get screened for the _opposite_ nucleotide. e.g. in this example the N (middle of the sequence) will be replaced by `A`.
+
+```
+TTGGTAGTAGGACTCGTTATTAATCCAAATAGGTTAATTGAGATAAGAGANGCTAGATTAAATCTATTACAAATTAACGAAAATAAAAGCTATACGGATT
+|||||||||||||||||||||||||||||||||||||||||||||||||| |||||||||||||||||||||||||||||||||||||||||||||||||
+TTGGTAGTAGGACTCGTTATTAATCCAAATAGGTTAATTGAGATAAGAGAAGCTAGATTAAATCTATTACAAATTAACGAAAATAAAAGCTATACGGATT
+```
 
--- impute.py
+++ impute.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python
+import warnings
+from Bio import BiopythonDeprecationWarning
 from Bio import SeqIO, SeqRecord, Seq
 from Bio.Alphabet import IUPAC
 from Bio.Blast import NCBIStandalone
 from Bio.Blast.Applications import NcbiblastnCommandline
 from Bio.Blast import NCBIXML
+warnings.simplefilter('ignore', BiopythonDeprecationWarning)
+import argparse
 
-INPUT_FILE='test.fasta'
-LEFT_WINDOW = 150
-RIGHT_WINDOW = 150
 E_VALUE_THRESH = 0.1
 
-def replaceN(str):
+def replaceN(str, quite = True):
 	"This functions performs a blast search of str to replace a nt at given position pos"
 	# export substring as fasta file
 	with open("query.fasta", "w") as fasta_query_file:
@@ -19,9 +20,6 @@ def replaceN(str):
 	# blast query file to database
 	blastn_cline = NcbiblastnCommandline(query='query.fasta', db="db/proteobacteria_rep.fasta", evalue=0.001, outfmt=5, out="query.xml")
 	stdout, stderr = blastn_cline()
-
-	print(stderr)
-	print(stdout)
 	result_handle = open("query.xml")
 	blast_records = NCBIXML.parse(result_handle)
 	for blast_record in blast_records:
@@ -31,12 +29,23 @@ def replaceN(str):
 					ambigous_pos_alignment = hsp.query.find('N')
 					if hsp.sbjct[ambigous_pos_alignment:ambigous_pos_alignment+1] != "-":
 						prediction = hsp.sbjct[ambigous_pos_alignment:ambigous_pos_alignment+1]
+						if not quite:
+							print(hsp.query[100:200])
+							print(hsp.match[100:200])
+							print(hsp.sbjct[100:200])
 					else:
 						prediction = "X" # there is no match
 						hsp.expect = -1
-	return(prediction, hsp.expect)
+	return(hsp.query[ambigous_pos_alignment:ambigous_pos_alignment+1], prediction, hsp.expect)
 
-fasta_sequences = SeqIO.parse(open(INPUT_FILE),'fasta')
+
+parser = argparse.ArgumentParser(description='Impute by blast')
+parser.add_argument('--input', help='path to input fasta file')
+parser.add_argument('--window', type=int, help='window size', default=300)
+
+args = parser.parse_args()
+
+fasta_sequences = SeqIO.parse(open(args.input),'fasta')
 
 # iterate ofer fasta sequences and search for N
 for fasta in fasta_sequences:
@@ -44,12 +53,23 @@ for fasta in fasta_sequences:
 	# loop over every N remaining in sequence and either replace to nucleotide or to X if there is no match
 	while(sequence.find('N') > -1):
 		ambigous_pos = sequence.find('N') # find the next N in the sequence, will be a -1 if no found
-		print("processing fasta header" + name)
-		print("ambigous sequence found at: " + str(ambigous_pos))
-		print("extracting neighborhood around position " + str(ambigous_pos))
-		subsequence = sequence[ambigous_pos - LEFT_WINDOW:ambigous_pos + RIGHT_WINDOW]
-		prediction, evalue = replaceN(subsequence)
-		print('pos: ' + str(ambigous_pos) + ' N > ' + prediction + ' (alignment evalue: ' + str(evalue) + ')')
+		#print("processing fasta header" + name)
+		#print("ambigous sequence found at: " + str(ambigous_pos))
+		#print("extracting neighborhood around position " + str(ambigous_pos))
+
+		# check if window would exeed the sequence
+		if ambigous_pos - args.window/2 < 0: 
+			left_window = ambigous_pos # reduce the window site if its too near on the beginning of the sequence
+		else:
+			left_window = args.window/2
+		if ambigous_pos + args.window/2 > len(sequence):
+			right_window = len(sequence) # reduce right window if window would exeed sequence length
+		else: 
+			right_window = args.window/2 
+
+		subsequence = sequence[ambigous_pos - left_window:ambigous_pos + right_window]
+		query, prediction, evalue = replaceN(subsequence, quite = True)
+		print('pos: ' + str(ambigous_pos) + ' ' + query + ' > ' + prediction + ' (alignment evalue: ' + str(evalue) + ')')
 		# correct sequence
 		sequence_list = list(sequence)
 		sequence_list[ambigous_pos] = prediction
